3de39105491aa6ebd570b2ca095377558e414e0a
Kristian Tud <kristiantud@hotmail.com>
2023-09-10 20:36:22 -0700

    added more things regarding routing

--- routing/routing/src/App.vue
+++ routing/routing/src/App.vue
@@ -1,5 +1,12 @@
 <template>
+  <div>
+    <router-link :to="{name: 'HomePage'}">Home</router-link>
+    <!--benefit of this: we don't have to worry about messing up the routing when we change the actual route in the browser (e.g. /about to /about-us)-->
+    <router-link to="/about">About</router-link>
+    <!--this is more efficient than simply requesting a new html page, like we would in a href -->
+  </div>
   <router-view />
+  <h1>this is the footer, it will stay the same</h1>
 </template>
 
 <script setup>
